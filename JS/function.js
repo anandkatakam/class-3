1a153e3353963946cf2b1edb8e23cf240fe2264f
Anand Katakam <anand@katakam.com>
2016-10-12 17:55:21 +0530

    NEW NEW SHIZ

---
+++ JS/function.js
@@ -0,0 +1,37 @@
+//this is js
+//console.log ("My JS is working")
+
+// In Javascript, there are five data types
+
+
+// A string
+var myString = "Hello Harvinder";
+//console.log(myString);
+
+// A number
+var myNumber = 24;
+// console.log(myNumber);
+// console.log(myNumber + 70)
+// console.log(myNumber.toString()+70)
+
+// An array for list
+var myArray = ["Prawesh", "Aloke", "My mom", "Sanshey"];
+// console.log(myArray);
+// console.log(myArray[2]);
+// console.log(myArray.length);
+myArray.push("Aloke")
+// console.log(myArray);
+myArray.push(myNumber);
+//console.log(myArray);
+
+// An object
+var myObject={
+	name: "Anand",
+	age: 25,
+	classmates: Array [4]
+}
+
+// A Boolean, true and false
+var myBool = true;
+
+
